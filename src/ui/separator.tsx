64e3589551e9a796643ad2eab49e908db6845ede
jeevi989413 <jeevithasenthil808@gmail.com>
2025-05-05 13:49:44 +0530

    checkout UI update

--- src/ui/separator.tsx
+++ src/ui/separator.tsx
@@ -1,9 +1,7 @@
-"use client";
-
+// components/ui/Separator.tsx
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 import * as React from "react";
-
-import { cn } from "../lib/utils";
+import { cn } from "@/lib/utils";
 
 const Separator = React.forwardRef<
 	React.ElementRef<typeof SeparatorPrimitive.Root>,
@@ -16,11 +14,11 @@ const Separator = React.forwardRef<
 		className={cn(
 			"bg-border shrink-0",
 			orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-			className || "",
+			className,
 		)}
 		{...props}
 	/>
 ));
-Separator.displayName = SeparatorPrimitive.Root.displayName;
+Separator.displayName = "Separator";
 
 export { Separator };
